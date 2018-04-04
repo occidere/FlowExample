e7df62aee9eccf037eb8f2542c30506c822a73bb
occidere <occidere@naver.com>
2018-04-04 16:53:57 +0900

    ignore .idea, .iml init commit of Publisher-Subscriber Flow

---
+++ src/main/java/reactive/FlowExample.java
@@ -0,0 +1,25 @@
+package reactive;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
+
+public class FlowExample {
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        SubmissionPublisher<String> publisher = new SubmissionPublisher<>(executor, Flow.defaultBufferSize());
+        MySubscriber<String> subscriber = new MySubscriber<>();
+
+        publisher.subscribe(subscriber);
+
+        Arrays.stream(Lyrics.getLyrics())
+                .forEach(publisher::submit);
+
+        publisher.close();
+        executor.shutdown();
+    }
+}
